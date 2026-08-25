10f6b5d541886a580fcfba75741508db80bde966
Zikun Chen <787741917@qq.com>
2026-08-25 19:24:59 +0800

    Update Multi-headed self-attention (Attention & Transformers 2/3)

--- model/multi_head_attention.py
+++ model/multi_head_attention.py
@@ -7,19 +7,12 @@ class MultiHeadedSelfAttention(nn.Module):
     def __init__(self, embedding_dim: int, attention_dim: int, num_heads: int):
         super().__init__()
         torch.manual_seed(0)
-        # Create num_heads SingleHeadAttention instances using nn.ModuleList
-        # Each head size = attention_dim // num_heads
-        # Use: self.SingleHeadAttention(embedding_dim, head_size)
-        # After the heads, add an output projection: nn.Linear(attention_dim, attention_dim, bias=False)
         self.att_heads = nn.ModuleList()
         for i in range(num_heads):
             self.att_heads.append(self.SingleHeadAttention(embedding_dim, attention_dim // num_heads))
         self.output_proj = nn.Linear(attention_dim, attention_dim, bias=False)
 
     def forward(self, embedded: TensorType[float]) -> TensorType[float]:
-        # Run each head on the input, concatenate outputs along dim=2
-        # Pass concatenated result through the output projection (W_O)
-        # Return result rounded to 4 decimal places
         head_outputs = []
         for head in self.att_heads:
             head_outputs.append(head(embedded))
